b584788d891e58e4f67a612ebfad767d08178f3e
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-02-11 12:55:19 -0300

    commit 11/02/2022

--- Desenvolvimento_de_Sistemas/cliente3b/src/main/java/br/com/sistema/controller/IndexController.java
+++ Desenvolvimento_de_Sistemas/cliente3b/src/main/java/br/com/sistema/controller/IndexController.java
@@ -1,13 +1,15 @@
 package br.com.sistema.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // O controller escuta tudo que vem do front-end
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/") // Cria Rota
+    public String index(ModelMap modelMap){
+        modelMap.addAttribute("nome", "BAtitesta");
         return "index";
     }
 
