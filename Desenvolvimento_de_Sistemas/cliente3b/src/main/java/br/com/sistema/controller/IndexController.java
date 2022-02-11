f4cfeb9039df5eb79d89caeb1ffa4ce920911c73
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-02-11 12:27:33 -0300

    commit 11/02/2022

--- Desenvolvimento_de_Sistemas/cliente3b/src/main/java/br/com/sistema/controller/IndexController.java
+++ Desenvolvimento_de_Sistemas/cliente3b/src/main/java/br/com/sistema/controller/IndexController.java
@@ -3,10 +3,10 @@ package br.com.sistema.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // Essa classe escuta tudo que vem do front-end
+@Controller // O controller escuta tudo que vem do front-end
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "index";
     }
