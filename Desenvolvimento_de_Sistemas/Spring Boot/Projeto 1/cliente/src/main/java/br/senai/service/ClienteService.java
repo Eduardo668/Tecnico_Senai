ab8b8668f637996eb66cbfcfa46fc949ce554709
Eduardo668 <eduardosehn20@gmail.com>
2022-04-20 12:18:56 -0300

    Update

---
+++ Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/service/ClienteService.java
@@ -0,0 +1,9 @@
+package br.senai.service;
+
+import br.senai.Model.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+    public List<Cliente>
+}
---
+++ Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/service/ClienteServiceImpl.java
@@ -0,0 +1,13 @@
+package br.senai.service;
+
+import br.senai.Model.Cliente;
+
+import java.util.List;
+
+public class ClienteServiceImpl extends ClienteService {
+    
+
+    public List<Cliente> findAll(){
+
+    }
+}
