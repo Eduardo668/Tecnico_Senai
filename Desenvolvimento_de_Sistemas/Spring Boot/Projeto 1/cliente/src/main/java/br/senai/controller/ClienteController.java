e66556feb7a5020a9b00e9186abdd1eadd12a753
Eduardo668 <eduardosehn20@gmail.com>
2022-04-20 12:33:49 -0300

    commit

--- Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/controller/ClienteController.java
+++ Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/controller/ClienteController.java
@@ -1,13 +1,21 @@
 package br.senai.controller;
 
 
+import br.senai.service.ClienteServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClienteController {
+
+    @Autowired
+    ClienteServiceImpl clienteService;
+
+
     @GetMapping("/cliente/list")
     public String findAll(){
+        System.out.println(clienteService.findAll());
         return "cliente/list";
     }
 
--- Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/service/ClienteServiceImpl.java
+++ Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/service/ClienteServiceImpl.java
@@ -1,13 +1,21 @@
 package br.senai.service;
 
 import br.senai.Model.Cliente;
+import br.senai.repository.ClienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
-public class ClienteServiceImpl extends ClienteService {
-    
+public class ClienteServiceImpl implements ClienteService {
+
+
+
+    @Autowired // Autoriza o repositorio a fazer algo
+    private ClienteRepositorio clienteRepositorio;
 
     public List<Cliente> findAll(){
-
+        return clienteRepositorio.findAll(Sort.by("email"));
     }
 }
