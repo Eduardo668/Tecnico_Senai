f7b57016c157e338f28d5b5ddd64e0d058a949da
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-04-20 12:57:38 -0300

    commit

--- Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/controller/ClienteController.java
+++ Desenvolvimento_de_Sistemas/Spring Boot/Projeto 1/cliente/src/main/java/br/senai/controller/ClienteController.java
@@ -1,10 +1,16 @@
 package br.senai.controller;
 
 
+import br.senai.Model.Cliente;
 import br.senai.service.ClienteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.ref.Cleaner;
+import java.util.List;
 
 @Controller
 public class ClienteController {
@@ -14,8 +20,11 @@ public class ClienteController {
 
 
     @GetMapping("/cliente/list")
-    public String findAll(){
+    public String findAll(ModelMap model){
         System.out.println(clienteService.findAll());
+        model.addAttribute("value",clienteService.findAll());
+        Cliente cliente = new Cliente();
+
         return "cliente/list";
     }
 
