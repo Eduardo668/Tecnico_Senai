c738ef154381dcccf7e750f23c582f72449a277c
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-04-29 12:24:16 -0300

    Commit avaliação trimestral

---
+++ Desenvolvimento_de_Sistemas/Avaliação-Trimestral-1-Parte2/avaliação-trimestral-java-spring/src/main/java/br/trimestral/senai/trimestral/repository/FornecedorRepository.java
@@ -0,0 +1,16 @@
+package br.trimestral.senai.trimestral.repository;
+
+import br.trimestral.senai.trimestral.model.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
+
+@Repository
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+    public Fornecedor findByNome(String nome);
+    public Fornecedor findByEmail(String email);
+    public Fornecedor findByTelefone(String telefone);
+
+
+}
